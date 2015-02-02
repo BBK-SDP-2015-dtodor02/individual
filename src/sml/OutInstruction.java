22899cea13eff67f78c869f43ab389143c01ca5a
mitCode <mail@mitcode.com>
2015-02-02 23:39:06 +0000

    OutInstruction

---
+++ src/sml/OutInstruction.java
@@ -0,0 +1,31 @@
+package sml;
+
+/**
+ * This class ....
+ * 
+ * @author someone
+ */
+
+public class OutInstruction extends Instruction {
+
+	private int register;
+
+	public OutInstruction(String label, String op) {
+		super(label, op);
+	}
+
+	public OutInstruction(String label, int register) {
+		this(label, "out");
+		this.register = register;
+	}
+
+	@Override
+	public void execute(Machine m) {
+		System.out.println(m.getRegisters().getRegister(register));
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " " + register;
+	}
+}
